7655297ded5215b94fcf74f8fbd42bdca3e27d2f
hailie <mwhailie@live.com>
2022-05-13 00:10:18 -0700

    fix

--- sync-rss.js
+++ sync-rss.js
@@ -246,7 +246,7 @@ async function fetchItem(link, category) {
     if (imdbInfo.length) {
       itemData[DB_PROPERTIES.IMDB_LINK] = 'https://www.imdb.com/title/' + imdbInfo[0].nextSibling.textContent.trim();
     }
-    const release_date = Array.from(document.querySelectorAll('#info [property="v:initialReleaseDate"]')).map(s => s.textContent.split('(')[0]).sort()[0]
+    const release_date = Array.from(dom.window.document.querySelectorAll('#info [property="v:initialReleaseDate"]')).map(s => s.textContent.split('(')[0]).sort()[0]
     console.log('release_date' + release_date);
     itemData[DB_PROPERTIES.MOVIE_RELEASE_DATE] = dayjs(release_date).format('YYYY-MM-DD');
     console.log('itemData[DB_PROPERTIES.MOVIE_RELEASE_DATE]' + itemData[DB_PROPERTIES.MOVIE_RELEASE_DATE]);
